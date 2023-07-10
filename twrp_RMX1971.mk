9306ce10d621a81d5c222ab3b281f370638fbe87
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-07-10 14:48:32 +0300

    Update twrp_RMX1971.mk

--- twrp_RMX1971.mk
+++ twrp_RMX1971.mk
@@ -17,9 +17,6 @@
 # Release name
 PRODUCT_RELEASE_NAME := RMX1971
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
 # Inherit from device
 $(call inherit-product, device/realme/RMX1971/device.mk)
 
